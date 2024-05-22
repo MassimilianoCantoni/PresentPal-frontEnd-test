2e641ced95e67944a9afe2e6c1d1e2a9e62646d0
MassimilianoCantoni <massicanto03@gmail.com>
2024-05-22 11:32:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+  var domande = ["Che budget hai a disposizione?", "Harry potter è Pippo Baudo?"];
+  var opzioni = [["€20","€30","€50","Personalizza"],["Certo","Che cosa dici","Non penso","Non saprei"]];
+  var i=0;
+
+  $(".buttonMain").click(function(){
+    $(this).toggleClass("buttonMainToggled")
+  });
+
+    $(".buttonMain").click(function(){
+      $(".mainApp").fadeOut("slow",function(){
+        $("#testoDomanda").text(domande[i]);
+        $(".buttonMain").removeClass("buttonMainToggled");
+        for(var j=0;j<3;j++)
+        {
+          $("#opzione"+(j+1)).text(opzioni[i][j]);
+        }
+        i=i+1; 
+        
+      });
+      
+      $(".mainApp").fadeIn("slow");
+      
+    });
+
+
+});
+  
